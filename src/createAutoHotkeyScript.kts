8d63da255238be19b80a13964996244bd696bf93
Frederik Kammel <frederik.kammel@tum.de>
2021-01-27 12:12:51 +0100

    Initial commit

---
+++ src/createAutoHotkeyScript.kts
@@ -0,0 +1,37 @@
+import org.languagetool.JLanguageTool
+import org.languagetool.language.GermanyGerman
+import java.io.File
+import java.io.FileReader
+import java.io.PrintStream
+import java.nio.charset.Charset
+
+val inputFile = File("wordLists/wikt/wn-wikt-deu.tab")
+val outputFile = File("src/FixGermanNouns.ahk")
+
+println("Input file: ${inputFile.absolutePath}")
+println("Output file: ${outputFile.absolutePath}")
+
+val lines = FileReader(inputFile, Charset.forName("UTF-8")).buffered().use { it.readLines() }
+val preprocessedLines = lines
+    .drop(1)
+    .map { it.split("\t")[2] }
+    .map { it.removeSuffix("-") }
+
+val languageTool = JLanguageTool(GermanyGerman())
+
+
+val processedLines = preprocessedLines
+    .filter { it[0].isUpperCase() }
+    .filter { languageTool.check(it).isEmpty() }
+    .filter { it.length <= 40 }
+    .map { "::${it.toLowerCase()}::$it" }
+
+PrintStream(outputFile, Charset.forName("UTF-8")).use { outputWriter ->
+    outputWriter.println("#NoEnv")
+    outputWriter.println("SendMode Input")
+    outputWriter.println("#SingleInstance, force")
+
+    processedLines.forEach { line ->
+        outputWriter.println(line)
+    }
+}
